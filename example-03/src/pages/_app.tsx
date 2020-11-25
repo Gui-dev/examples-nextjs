156c183ab8ff7853476d45ea6f1e465208540494
Gui-dev <adrianon10@gmail.com>
2020-11-25 02:40:49 -0300

    feat: add global style

--- example-03/src/pages/_app.tsx
+++ example-03/src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle, theme } from './../styles/GlobalStyle'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={ theme }>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
